2d6ad62c46ea484b85eacb49da48a730fb5dfef4
Vinicius Mabellini <vpmabellini@gmail.com>
2018-07-11 23:59:29 -0300

    Finishing module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -8,7 +11,12 @@
     name: 'BookList',
     data() {
       return { 
-        title: 'All Books' 
+        title: 'All Books',
+        books: [
+            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+            {title: 'American Gods', author: 'Neil Gaiman'},
+            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+        ]
       };
     }
   };
@@ -16,5 +24,13 @@
 <style scoped>
 h1, h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
